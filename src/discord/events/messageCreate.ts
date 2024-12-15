e135b14b5c2da8607feb3f8345975391439fa3d4
KingsDev <63374258+KingsMMA@users.noreply.github.com>
2024-12-15 20:02:26 +1000

    Implemented earnings on message create

---
+++ src/discord/events/messageCreate.ts
@@ -0,0 +1,33 @@
+import type { GuildTextBasedChannel, Message } from 'discord.js';
+
+import type EconomyBot from '../economyBot';
+
+export default class {
+
+    client: EconomyBot;
+
+    constructor(client: EconomyBot) {
+        this.client = client;
+    }
+
+    async run(message: Message) {
+        if (message.author.bot) return;
+        if (!message.guild) return;
+
+        const formula = process.env.MESSAGE_REWARD_FORMULA!
+            .replaceAll('%length%', message.content.length.toString());
+        const reward = eval(formula);
+        if (!reward) return;
+
+        if (!this.client.serverCache[message.guild.id]) {
+            this.client.serverCache[message.guild.id] = {};
+        }
+
+        if (!this.client.serverCache[message.guild.id][message.author.id]) {
+            this.client.serverCache[message.guild.id][message.author.id] = Number(process.env.INITIAL_BALANCE);
+        }
+
+        this.client.serverCache[message.guild.id][message.author.id] += reward;
+    }
+
+}
